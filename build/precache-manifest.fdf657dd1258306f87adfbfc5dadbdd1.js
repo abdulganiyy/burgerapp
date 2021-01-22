7ea00dae280d761aa6c965c358ea2d1a265b55f3
abdulganiyy <lasisibalogunopeoluwa@ymail.com>
2021-01-22 10:14:10 +0000

    build files added

---
+++ build/precache-manifest.fdf657dd1258306f87adfbfc5dadbdd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e4b2ba008739b43a0238ca62b46a94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae3599af6789ffbe96c2",
+    "url": "/static/css/main.61969e2b.chunk.css"
+  },
+  {
+    "revision": "b7f6f8151083d6c8c941",
+    "url": "/static/js/2.e4fd8497.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e4fd8497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3599af6789ffbe96c2",
+    "url": "/static/js/main.335452e4.chunk.js"
+  },
+  {
+    "revision": "08c49aac8c00567a29d6",
+    "url": "/static/js/runtime-main.59a75140.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
